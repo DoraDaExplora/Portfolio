bd94cf4e1495a140693d6997aaf950f8425c7413
Dmitry Nikulin <russiankenny@gmail.com>
2020-01-24 20:11:44 +0300

    Fake backend.

--- Tests/pakhomov_test/src/_helpers/fakeBackend.ts
+++ Tests/pakhomov_test/src/_helpers/fakeBackend.ts
@@ -1,10 +1,13 @@
-let users = JSON.parse(localStorage.getItem('users')!) || []; //Восклицательный знак для того, чтобы не было одного заковыристого эррора. Мы делаем вид, что уверены в невозможности возвращения null
+let users = JSON.parse(localStorage.getItem('users')) || []; //Восклицательный знак для того, чтобы не было одного заковыристого эррора. Мы делаем вид, что уверены в невозможности возвращения null
     
 export function configureFakeBackend() {
     window.fetch = function (url, opts) {
         return new Promise<any>((resolve, reject) => {
             setTimeout(() => {
-                let params = JSON.parse(JSON.stringify(opts.body));
+
+                let params = JSON.parse(JSON.stringify((opts.body)));
+
+                //auth
                 let filteredUsers = users.filter((user: any) => {
                     return user.userName === params.userName && user.userPassword === params.userPassword;
                 });
@@ -14,14 +17,15 @@ export function configureFakeBackend() {
                     let responseJson = {
                         id: user.id,
                         userName: user.userName,
+                        userPassword: user.userPassword,
                         token: '86fasfgfsogHGad'
                     };
-                    resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(responseJson)) });
+                    resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(responseJson)).then(() => console.log('logged in lol')) });
                 } else {
                     reject('Почта или пароль неверны');
                 }
-
-                return;
+                
+                    return;
             }, 500);
         });
     }
