bc0c030a674f256f815173c8f9e4d7709cb94b87
Dmitry Nikulin <russiankenny@gmail.com>
2020-01-29 16:23:04 +0300

    UserService now works properly.

--- Tests/pakhomov_test/src/_services/userService.ts
+++ Tests/pakhomov_test/src/_services/userService.ts
@@ -9,34 +9,19 @@ function login(userName, userPassword) {
         body: JSON.stringify({ userName, userPassword })
     };
 
-    return fetch(`http://localhost:3000/`, requestOptions)
+    return fetch(`http://localhost:3000/users/authenticate`, requestOptions)
         .then(handleResponse)
         .then(user => {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('user', JSON.stringify(user));
+            localStorage.setItem('savedUser', JSON.stringify(user));
             return user;
         });
-}
-
-function register(user) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
-
-    return fetch(`http://localhost:3000/`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
         if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                console.log('Oh no!');
-            }
-
             const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
