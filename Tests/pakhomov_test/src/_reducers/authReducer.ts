52afb99df76cdfe98876c621f2edf165749fa46b
Dmitry Nikulin <russiankenny@gmail.com>
2020-01-31 17:00:54 +0300

    Just in case.

--- Tests/pakhomov_test/src/_reducers/authReducer.ts
+++ Tests/pakhomov_test/src/_reducers/authReducer.ts
@@ -7,14 +7,15 @@ export function authentication(state = initialState, action: any) {
   switch (action.type) {
     case userConstants.LOGIN_REQUEST:
       return {
+        loggedIn: false,
         loggingIn: true,
         user: action.user,
       };
     case userConstants.LOGIN_SUCCESS:
       return {
         loggedIn: true,
-        user: action.user,
-        redirectToMain: true
+        loggingIn: false,
+        user: action.user
       };
     case userConstants.LOGIN_FAILURE:
       return {};
